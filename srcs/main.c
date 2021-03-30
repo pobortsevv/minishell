b7d983a4dad4e5ba34f621230adfedf3fb27ad15
sabra <sabra@MacBook-Air-Vladimir.local>
2021-03-30 14:04:25 +0300

    Начинаю делать пайпы

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sabra <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/28 21:45:13 by sabra             #+#    #+#             */
-/*   Updated: 2021/03/30 09:49:27 by mlaureen         ###   ########.fr       */
+/*   Updated: 2021/03/30 14:03:40 by sabra            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,10 +60,9 @@ int			init_command(t_cmd *cmd, char **envp)
 
 char			**ft_exec_cmd(t_cmd *ar_cmd, char **env, int cmd_count)
 {
-	int i;
-
-	i = 0;
-	while (i < cmd_count)
+	//if (cmd_count > 1)
+		//return (ft_exec_pipe(ar_cmd, env, cmd_count));
+	if (cmd_count == 1)
 	{
 		if ((ft_strncmp(ar_cmd[i].args[0], "unset", ft_strlen(ar_cmd[i].args[0]))) == 0)
 			env = ft_unset(&ar_cmd[i], env);
@@ -79,7 +78,6 @@ char			**ft_exec_cmd(t_cmd *ar_cmd, char **env, int cmd_count)
 			ft_putstr_fd(ar_cmd[i].args[0], ar_cmd[i].out);
 			ft_putendl_fd(": command not found", ar_cmd[i].out); 
 		}
-		i++;
 	}
 	return (env);
 }
