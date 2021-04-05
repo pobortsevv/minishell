a76375fe26cbb7d965b76d241005d52dae13d8c9
Slow Abra <sabra@ga-o1.msk.21-school.ru>
2021-04-05 18:20:40 +0300

    pipes done

--- srcs/ft_exec_pipe.c
+++ srcs/ft_exec_pipe.c
@@ -6,7 +6,7 @@
 /*   By: sabra <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 14:05:37 by sabra             #+#    #+#             */
-/*   Updated: 2021/04/04 15:23:33 by sabra            ###   ########.fr       */
+/*   Updated: 2021/04/05 18:11:43 by sabra            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 t_cmd 	*close_files(t_cmd *ar_cmd, int i, int cmd_count)
 {
+	//void(cmd_count);
 	if (i > 0 && ar_cmd[i].in != 0 && ar_cmd[i - 1].out != 1)
-		close(1);
-	if (i != cmd_count - 1 && ar_cmd[i].out != 1 
+		close(ar_cmd[i - 1].out);
+	if (i + 1 < cmd_count && ar_cmd[i].out != 1 
 				&& ar_cmd[i + 1].in != 0)
-		close(0);
+		close(ar_cmd[i + 1].in);
 	return (ar_cmd);
 }
 
@@ -63,22 +64,13 @@ char	**ft_exec_pipe(t_cmd *ar_cmd, char **env, int cmd_count)
 	errno = 0;
 	path = ft_var_find("PATH", env);
 	shell.status = 0;
+	signal(SIGINT, sig_stub);
 	while (i < cmd_count)
 	{
-		if (i == 0)
-			dup2(shell.in_tmp, 0);
 		if (ar_cmd[i].in != 0 && shell.status == 0)
-		{
 			dup2(ar_cmd[i].in, 0);
-			close(ar_cmd[i].in);
-		}
 		if (ar_cmd[i].out != 1 && shell.status == 0)
-		{
 			dup2(ar_cmd[i].out, 1);
-			close(ar_cmd[i].out);
-		}
-		if (ar_cmd[i].out == 1 && shell.status == 0)
-			dup2(shell.out_tmp, 1);
 		if ((pid = fork()) == -1)
 		{
 			shell.status = errno;
@@ -87,14 +79,13 @@ char	**ft_exec_pipe(t_cmd *ar_cmd, char **env, int cmd_count)
 		}
 		if (pid == 0)
 		{
-			sig_init();
+			close_files(ar_cmd, i, cmd_count);
 			shell.status = init_cmd_pipe(&ar_cmd[i], env);
 			if (shell.status != 127)
 				exit(shell.status);
 			line = ft_find_bin(ar_cmd[i].args[0], path);
 			if (line)
 			{
-				//close_files(ar_cmd, i, cmd_count);
 				shell.status = execve(line, &ar_cmd[i].args[0], env);
 			}
 			if (line)
@@ -105,13 +96,21 @@ char	**ft_exec_pipe(t_cmd *ar_cmd, char **env, int cmd_count)
 				exit(127);
 			}
 		}
-		close(0);
-		close(1);
+		if (ar_cmd[i].in != 0)
+		{
+			close(ar_cmd[i].in);
+			dup2(shell.in_tmp, 0);
+		}
+		if (ar_cmd[i].in != 1)
+		{
+			close(ar_cmd[i].out);
+			dup2(shell.out_tmp, 1);
+		}
 		i++;
 	}
-	while (cmd_count-- > 0)
-		wait(&shell.status);
-	dup2(shell.in_tmp, 0);
-	dup2(shell.out_tmp, 1);
+	while (waitpid(pid, &shell.status, 0) > 0)
+		;
+	while (wait(NULL) > 0)
+		;
 	return (env);
 }
