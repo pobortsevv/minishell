79f098286aac03faa0dfa18017e383f6f998af6e
Magda Laureen <mlaureen@ga-o3.msk.21-school.ru>
2021-04-07 15:48:30 +0300

    norm ft_parser_str.c  удалила не нужный файл  Changes to be committed: 	modified:   srcs/parser/ft_parser_str.c 	deleted:    srcs/parser/ft_parser_str2.c

--- srcs/parser/ft_parser_str.c
+++ srcs/parser/ft_parser_str.c
@@ -6,24 +6,26 @@
 /*   By: mlaureen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/17 12:04:21 by mlaureen          #+#    #+#             */
-/*   Updated: 2021/04/07 11:44:19 by mlaureen         ###   ########.fr       */
+/*   Updated: 2021/04/07 15:42:18 by mlaureen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_minishell.h"
-#include  <stdio.h>
 
-// проверка символа и установка флагов
-// 	Сделано: если " ' -  то в итоговой строке их не должно быть (e'h'c"o"  == echo)
-// 	TODO если:
-//		export ec="echo"
-//		$ec tar
-//		"$ec" r
-//		рабочие варианты
-// 	Сделано: если \ - то в итоговой строке его нет (e\cho == echo), но  "e\cho" - ошибка - e\cho 
-// 	TODO если $NAME - то замена 
+static void	ft_dollar_c(char *str, char **temp, int *i, int *j)
+{
+	while (str[*i] != '\0' && str[(*i) - 1] != 92 && str[*i] != ' '
+			&& str[*i] != 34 && str[*i] != 39 && str[*i] != '$'
+			&& str[*i] != '=')
+	{
+		(*temp)[*j] = str[(*i)];
+		(*j)++;
+		(*i)++;
+	}
+	return ;
+}
 
-char	*ft_dollar (char *str, int *i, char **envp)
+char		*ft_dollar(char *str, int *i, char **envp)
 {
 	char	*res;
 	char	*temp;
@@ -33,22 +35,16 @@ char	*ft_dollar (char *str, int *i, char **envp)
 	res = NULL;
 	temp = (char *)malloc(sizeof(char) * ft_strlen(str) + 1);
 	(*i)++;
-	while (str[*i] != '\0' && str[(*i) - 1] != 92 && str[*i] != ' '
-			&& str[*i] != 34 && str[*i] != 39 && str[*i] != '$' && str[*i] != '=')
-	{
-		temp[j] = str[(*i)];
-		j++;
-		(*i)++;
-	}
+	ft_dollar_c(str, &temp, i, &j);
 	temp[j] = '\0';
 	if (temp[0] != '\0' && (res = ft_var_find(&temp[0], envp)) == NULL)
 	{
 		res = (char *)malloc(sizeof(char));
 		res[0] = '\0';
 	}
-	if  (temp[0] == '\0')
+	if (temp[0] == '\0')
 	{
-		res = (char *)malloc(sizeof(char)*2);
+		res = (char *)malloc(sizeof(char) * 2);
 		res[0] = '$';
 		res[1] = '\0';
 	}
@@ -70,62 +66,41 @@ static int	ft_in_out(char *a, int *in, int *out)
 	return (0);
 }
 
-char	**ft_make_norm(char **ar, int *in, int *out, char **envp)
+static char	**init_ft_make_norm(char **ar, int *i, int *j, char ***res)
 {
 	int		len;
+
+	(*i) = 0;
+	(*j) = 0;
+	len = ft_lenarray(ar);
+	if (len <= 0
+			|| !((*res) = (char **)malloc(sizeof(char *) * len + 1)))
+		return (NULL);
+	(*res)[len] = NULL;
+	(*res)[0] = NULL;
+	return (*res);
+}
+
+char		**ft_make_norm(char **ar, int *in, int *out, char **envp)
+{
 	char	**res;
 	int		flag;
 	int		i;
 	int		j;
 
-	flag = 0;
-	i = 0;
-	j = 0;
-	len = ft_lenarray(ar);
-	if (len <= 0
-			|| !(res = (char **)malloc(sizeof(char *) * len + 1)))
-		return (NULL);
-	res[len] = NULL;
-	res[0] = NULL;
-	//printf("%p; %p\n", &res[0], &res[len]);
-	//printf("%p\n", res);
-	//res[0] = ft_res0(ar[0], &flag, envp);
-	//if (res[0] == NULL)
-	//	return ((ft_parser_err_free2("comman not found (127)\n", res)));
+	init_ft_make_norm(ar, &i, &j, &res);
 	while (ar[i] != NULL)
 	{
 		flag = 0;
 		if (ar[i][0] == '<' || ar[i][0] == '>')
-		{
 			ft_in_out(ar[i], in, out);
-			/*
-			if (ft_in_out(ar[i], in, out) == 1)
-				{
-					free(res);
-					res = NULL;
-					return (NULL);
-				};
-				*/
-		}
 		else
-		{
-			if (j == 0)
-				res[0] = ft_res_arg(ar[i], &flag, envp);
-				//res[0] = ft_res0(ar[i], &flag, envp);
-			else 
-				res[j] = ft_res_arg(ar[i], &flag, envp);
-			j++;
-		}
+			res[j++] = ft_res_arg(ar[i], &flag, envp);
 		i++;
 	}
 	if (res[0] == NULL)
 		return ((ft_parser_free2(res)));
 	while (j <= i)
-	{
-		res[j] = NULL;
-		j++;
-	}
-//	printf("нормированный массив\n");
-//	ft_print_array_2(res);
+		res[j++] = NULL;
 	return (res);
 }
