96f9f6bf338eba4f62f3964a3138f41174630d85
Magda Laureen <mlaureen@ga-o3.msk.21-school.ru>
2021-04-08 13:36:33 +0300

    fix norm  on mlaureen-file

--- srcs/test/print_test.c
+++ srcs/test/print_test.c
@@ -6,7 +6,7 @@
 /*   By: mlaureen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 08:35:33 by mlaureen          #+#    #+#             */
-/*   Updated: 2021/03/31 13:01:29 by mlaureen         ###   ########.fr       */
+/*   Updated: 2021/04/08 13:32:56 by mlaureen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,15 +32,13 @@ void		ft_print_array_3(char ***ar)
 	return ;
 }
 
-// тест-функция: печать из "списка команд"
-
 void		ft_m_test_pr(t_list *first)
 {
 	t_list	*t;
 	t_cmd	*ex;
 	int		i;
 
-	t=first;
+	t = first;
 	while (t != NULL)
 	{
 		i = 0;
@@ -57,22 +55,20 @@ void		ft_m_test_pr(t_list *first)
 	return ;
 }
 
-// тест-функция: печать из двухмерного массива строк, ограниченного NULL
-
-void	ft_print_array_2(char ** arr)
+void		ft_print_array_2(char **arr)
 {
 	int		i;
 
 	i = 0;
-	while(arr[i] != NULL)
+	while (arr[i] != NULL)
 	{
-		printf("%s \n",arr[i]);
+		printf("%s \n", arr[i]);
 		i++;
 	}
 	return ;
 }
 
-void	ft_print_array_t_cmd(t_cmd *ar_t_cmd, int len)
+void		ft_print_array_t_cmd(t_cmd *ar_t_cmd, int len)
 {
 	int		u;
 	int		j;
@@ -82,24 +78,18 @@ void	ft_print_array_t_cmd(t_cmd *ar_t_cmd, int len)
 	while (u < len)
 	{
 		j = 0;
-		//while(j < ar_t_cmd[u].len_args)
-		printf("len=%d\n",ar_t_cmd[u].len_args);
-		while(ar_t_cmd[u].args[j] != NULL)
+		printf("len=%d\n", ar_t_cmd[u].len_args);
+		while (ar_t_cmd[u].args[j] != NULL)
 		{
 			if (ar_t_cmd[u].args[j] != NULL)
-			{
-	//			printf("DFGDGD u=%d, j=%d\n", u,j);
-				printf("ar_cmd[%d].args[%d]\n = <%s>\n", u, j, ar_t_cmd[u].args[j]);
-			}
+				printf("ar_cmd[%d].args[%d]\n = <%s>\n", u, j,
+						ar_t_cmd[u].args[j]);
 			j++;
 		}
-	//	printf("печатаю in out\n");
-	//	printf("u=%d", u);
-		printf("ar_cmd[%d].in=%d\n",  u, ar_t_cmd[u].in);
-		printf("ar_cmd[%d].out=%d\n",  u, ar_t_cmd[u].out);
+		printf("ar_cmd[%d].in=%d\n", u, ar_t_cmd[u].in);
+		printf("ar_cmd[%d].out=%d\n", u, ar_t_cmd[u].out);
 		u++;
 	}
-//	read (0,0,1);
 	printf("массив напечатан\n");
 	return ;
 }
--- srcs/utilites/ft_array_to_str.c
+++ srcs/utilites/ft_array_to_str.c
@@ -6,7 +6,7 @@
 /*   By: mlaureen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 09:20:07 by mlaureen          #+#    #+#             */
-/*   Updated: 2021/03/31 14:46:21 by mlaureen         ###   ########.fr       */
+/*   Updated: 2021/04/08 13:34:01 by mlaureen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,13 @@ char	*ft_array_to_str(char **array)
 	temp = NULL;
 	res = (char *)malloc(sizeof(char) * 1);
 	res[0] = '\0';
-//	printf ("22222\n");
-//	printf("ibefore %s\n", array[i]);
 	if (array[i] != NULL)
 	{
 		temp = res;
-//		printf("in cycle %s\n", array[i]);
 		res = ft_strjoin(res, array[i]);
 		free(temp);
 		i++;
 	}
-//	printf("111\n");
 	while (array[i] != NULL)
 	{
 		temp = res;
